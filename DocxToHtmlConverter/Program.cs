d9dc4673c6b694e1b8b1149e29a1db970b14c82a
Sergey Slepov <slepov@gmail.com>
2019-12-08 17:46:13 +0000

    Лишние знаки ударения в статьях душа, город, девять

--- DocxToHtmlConverter/Program.cs
+++ DocxToHtmlConverter/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -123,6 +123,7 @@ normal'>собир.</i>)<o:p></o:p></span></u></p>
             text = text.Replace("сн-нсв", "св-нсв");
             text = Regex.Replace(text, @"(\d)" + Regex.Escape("<sup>*</sup>"), "$1°");
             text = text.Replace("дать</b> св △ b/c':́ <i>", "дать</b> св △ b/c': <i>");
+            text = text.Replace("✧ ́за́", "✧ за́"); // лишний знак ударения в статье город
 
             File.WriteAllText("all.txt", text);
 
